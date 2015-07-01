51dd1fb2eccd35d4f4b1fb9648b0dd7313f3d540
overclokk <overclokk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-01 15:57:13 +0000

    Updated to 2.2.2
    git-svn-id: https://plugins.svn.wordpress.org/italy-cookie-choices/tags/2.2.2@1190873 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Fired when the plugin is uninstalled.
+ *
+ * @package   Italy Cookie Choices
+ * @author    Enea Overclokk, Andrea Pernici, Andrea Cardinale
+ * @license   GPLv2 or later
+ * @link      https://github.com/ItalyCookieChoices/italy-cookie-choices
+ * 
+ */
+/**
+ * If uninstall not called from WordPress, then exit
+ */
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+if ( is_multisite() ) {
+
+	global $wpdb;
+
+	$blogs = $wpdb->get_results( "SELECT blog_id FROM {$wpdb->blogs}", ARRAY_A );
+
+	// delete_transient( 'TRANSIENT_NAME' );
+	delete_option('italy_cookie_choices');
+
+	if ( $blogs )
+		foreach ( $blogs as $blog ) {
+
+			switch_to_blog( $blog[ 'blog_id' ] );
+
+			// delete_transient( 'TRANSIENT_NAME' );
+			delete_option('italy_cookie_choices');
+
+			restore_current_blog();
+
+		}
+
+} else {
+
+	delete_option('italy_cookie_choices');
+
+	// delete_transient( 'TRANSIENT_NAME' );
+	delete_option('italy_cookie_choices');
+
+}
+
+/**
+ * Require multilingual functions
+ */
+require( plugin_dir_path( __FILE__ ) . 'includes/functions-italy-cookie-choices-lang.php');
+deregister_string( 'Italy Cookie Choices', 'Banner text');
+deregister_string( 'Italy Cookie Choices', 'Banner url' );
+deregister_string( 'Italy Cookie Choices', 'Banner anchor text' );
+deregister_string( 'Italy Cookie Choices', 'Banner button text' );
